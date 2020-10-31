682062f389d2f88675793a1e9b679b17172eaae7
mbustamante2020 <mbustama@uoc.edu>
2020-10-31 16:56:12 +0100

    Ejercicio 6: Animaciones Paralax

--- app/src/main/java/edu/uoc/pac2/ui/BookDetailActivity.kt
+++ app/src/main/java/edu/uoc/pac2/ui/BookDetailActivity.kt
@@ -1,13 +1,13 @@
 package edu.uoc.pac2.ui
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NavUtils
-import com.google.android.material.snackbar.Snackbar
+import androidx.core.widget.NestedScrollView
 import edu.uoc.pac2.R
 import kotlinx.android.synthetic.main.activity_book_detail.*
 
@@ -21,19 +21,6 @@ class BookDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_book_detail)
 
         setSupportActionBar(detail_toolbar)
-
-        fab.setOnClickListener { view ->
-            //Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
-            //        .setAction("Action", null).show()
-
-            val intent= Intent()
-            intent.action=Intent.ACTION_SEND
-            intent.putExtra(Intent.EXTRA_TEXT,"Hey Check out this Great app:")
-            intent.type="text/plain"
-            startActivity(Intent.createChooser(intent,"Share To:"))
-
-        }
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
 
@@ -49,12 +36,50 @@ class BookDetailActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             // Create the detail fragment and add it to the activity
             // using a fragment transaction.
-            val itemID = intent.getIntExtra(BookDetailFragment.ARG_ITEM_ID, -1)
+           /* val itemID = intent.getIntExtra(BookDetailFragment.ARG_ITEM_ID, -1)
             val fragment = BookDetailFragment.newInstance(itemID)
+
             supportFragmentManager.beginTransaction()
                     .add(R.id.frameLayout, fragment)
+                    .commit()*/
+
+            /*
+            val arguments = Bundle()
+            arguments.putString(BookDetailFragment.ARG_ITEM_ID, intent.getStringExtra(BookDetailFragment.ARG_ITEM_ID))
+
+            val fragment = BookDetailFragment()
+            fragment.arguments = arguments
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.book_detail_container, fragment)
+                    .commit()
+                    */
+
+            val itemID = intent.getIntExtra(BookDetailFragment.ARG_ITEM_ID, -1)
+            val fragment = BookDetailFragment.newInstance(itemID)
+
+            Log.i("fragment act", "$itemID")
+
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.book_detail_container, fragment)
                     .commit()
         }
+
+
+
+        /* fab.setOnClickListener { view ->
+     //Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
+     //        .setAction("Action", null).show()
+
+     val intent = Intent().apply {
+         this.action = Intent.ACTION_SEND
+         this.putExtra(Intent.EXTRA_TEXT, "Hey Check out this Great app:")
+         this.type="text/plain"
+
+     }
+     startActivity(intent)
+     //startActivity(Intent.createChooser(intent, "Share To:"))
+
+ }*/
     }
 
     // TODO: Override finish animation for actionbar back arrow
--- app/src/main/java/edu/uoc/pac2/ui/BookDetailFragment.kt
+++ app/src/main/java/edu/uoc/pac2/ui/BookDetailFragment.kt
@@ -1,23 +1,20 @@
 package edu.uoc.pac2.ui
 
-import android.app.Application
-import android.content.Context
-import android.graphics.BitmapFactory
+import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.squareup.picasso.Picasso
-import edu.uoc.pac2.MyApplication
 import edu.uoc.pac2.R
 import edu.uoc.pac2.data.ApplicationDatabase
 import edu.uoc.pac2.data.Book
-import edu.uoc.pac2.data.BookDao
 import edu.uoc.pac2.data.BooksInteractor
 import kotlinx.android.synthetic.main.activity_book_detail.*
 import kotlinx.android.synthetic.main.fragment_book_detail.*
-import kotlinx.coroutines.runBlocking
 
 /**
  * A fragment representing a single Book detail screen.
@@ -33,17 +30,35 @@ class BookDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // Get Book for this detail screen
+
         loadBook()
     }
 
 
     // TODO: Get Book for the given {@param ARG_ITEM_ID} Book id
     private fun loadBook() {
-        runBlocking {
-            val id = (arguments?.get(ARG_ITEM_ID) ?: -1) as Int
-            val book = BooksInteractor(context?.let { ApplicationDatabase.getInstance(it).bookDao() }!!).getBookById(id) as Book
-            initUI(book)
+
+        if (arguments?.containsKey(ARG_ITEM_ID)!!) {
+            //val id:Int? = arguments?.getInt(ARG_ITEM_ID)
+            var id:Int? = ((arguments?.get(ARG_ITEM_ID) ?: -1) as Int?)
+
+            Log.i("fragment", "fragment $id")
+
+            //id = ((id == null) ?: -1) as Int?
+            val book = BooksInteractor(context?.let { ApplicationDatabase.getInstance(it).bookDao() }!!).getBookById(id!!) as Book
+            //AsyncTask.execute {
+                initUI(book)
+            //}
         }
+
+
+        //val id = (arguments?.get(ARG_ITEM_ID) ?: "-1") as String
+
+        /* as Int
+        val book = BooksInteractor(context?.let { ApplicationDatabase.getInstance(it).bookDao() }!!).getBookById(id) as Book
+        AsyncTask.execute {
+            initUI(book)
+        }*/
     }
 
     // TODO: Init UI with book details
@@ -51,17 +66,24 @@ class BookDetailFragment : Fragment() {
         activity?.toolbar_layout?.title = book.title
         Picasso.with(context).load(book.urlImage).into(activity?.toolbar_image)
 
-
-
         book_author.text = book.author
         book_date.text = book.publicationDate
         book_detail.text = book.description
         Picasso.with(context).load(book.urlImage).into(book_image)
+
+        activity?.fab?.setOnClickListener {
+            shareContent(book)
+        }
     }
 
     // TODO: Share Book Title and Image URL
     private fun shareContent(book: Book) {
-       // throw NotImplementedError()
+        val intent = Intent().apply {
+            this.action = Intent.ACTION_SEND
+            this.putExtra(Intent.EXTRA_TEXT, "Title: ${book.title} \nImage URL: ${book.urlImage}")
+            this.type="text/plain"
+        }
+        startActivity(intent)
     }
 
     companion object {
